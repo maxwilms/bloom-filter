de65266c68165eb3e43937374a1f6135a98614fb
Maximilian Wilms <max@bildschirmflackern.de>
2015-09-15 11:50:06 +0200

    adding behat tests

---
+++ features/bloom-filter.feature
@@ -0,0 +1,17 @@
+Feature: BloomFilter
+  In order to confirm that an element is not member of a set
+  As a developer
+  I need a very fast filter
+
+  Scenario: BloomFilter confirms a value is in set
+    Given a BloomFilter for "2" elements with probability ".01" for a false-positive
+    When I insert "foo"
+    And I insert "bar"
+    Then it should confirm "foo" is in set
+
+  Scenario: Bloom Filter confirms a value is not in set
+    Given a BloomFilter for "2" elements with probability ".001" for a false-positive
+    When I insert "foo"
+    And I insert "bar"
+    Then it may confirm "baz" is not in set
+s
---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,69 @@
+<?php
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use maxwilms\BloomFilter\BloomFilter;
+use maxwilms\BloomFilter\BloomFilterGenerator;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context, SnippetAcceptingContext
+{
+
+    /**
+     * @var BloomFilter
+     */
+    protected $bloomFilter;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+    
+
+    /**
+     * @Given a BloomFilter for :elements elements with probability :probability for a false-positive
+     */
+    public function aBloomfilterForElementsWithProbabilityForAFalsePositive($elements, $probability)
+    {
+        $this->bloomFilter = BloomFilterGenerator::generate($elements, $probability);
+    }
+
+    /**
+     * @When I insert :item
+     */
+    public function iInsert($item)
+    {
+        $this->bloomFilter->add($item);
+    }
+
+    /**
+     * @Then it should confirm :item is in set
+     */
+    public function itShouldConfirmIsInSet($item)
+    {
+        if (!$this->bloomFilter->contains($item)) {
+            throw new Exception("");
+        }
+    }
+
+    /**
+     * @Then it may confirm :item is not in set
+     */
+    public function itMayConfirmIsNotInSet($item)
+    {
+        ; // all fine!
+    }
+
+
+}
